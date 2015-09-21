887bd54dc8c6b7c38eda4c9996b6e7bcb31927c4
SzwedzikPL <szwedzikpl@gmail.com>
2015-09-21 10:29:53 +0200

    searchlight

---
+++ addons/searchlight/script_component.hpp
@@ -0,0 +1,12 @@
+#define COMPONENT searchlight
+#include "\z\a3cs\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_SEARCHLIGHT
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_SEARCHLIGHT
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_CORE
+#endif
+
+#include "\z\a3cs\addons\main\script_macros.hpp"
