3fc408b6954089a15bb8b81b979eb5a03f0ec1c5
SzwedzikPL <szwedzikpl@gmail.com>
2015-09-21 10:37:24 +0200

    smoke module - better smoke grenades

---
+++ addons/smoke/config.cpp
@@ -0,0 +1,62 @@
+#include "script_component.hpp"
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"a3cs_main", "a3cs_common"};
+        version = A3CS_VERSION;
+        versionStr = QUOTE(A3CS_VERSION);
+        versionAr[] = A3CS_VERSION_ARRAY;
+        author[] = {"SzwedzikPL", "ACE3 Team"};
+        authorUrl = "http://arma3coop.pl";
+    };
+};
+
+class CfgCloudlets
+{
+	class Default;
+	class SmokeShellWhite: Default
+	{
+		animationSpeedCoef = 1;
+		colorCoef[] = {"colorR","colorG","colorB","colorA"};
+		sizeCoef = 1;
+		position[] = {0,0,0};
+		interval = 0.00525;
+		circleRadius = 0;
+		circleVelocity[] = {0,0,0};
+		particleShape = "\A3\data_f\ParticleEffects\Universal\Universal";
+		particleFSNtieth = 16;
+		particleFSIndex = 7;
+		particleFSFrameCount = 48;
+		particleFSLoop = 1;
+		angleVar = 0.1;
+		animationName = "";
+		particleType = "Billboard";
+		timerPeriod = 1;
+		lifeTime = 21;
+		moveVelocity[] = {0.01,0.32,0};
+		rotationVelocity = 1;
+		weight = 1.2777;
+		volume = 1;
+		rubbing = 0.12;
+		size[] = {0.1,2,6};
+		color[] = {{ 0.6,0.6,0.6,0.2 },{ 0.6,0.6,0.6,0.05 },{ 0.6,0.6,0.6,0 }};
+		animationSpeed[] = {1.5,0.5};
+		randomDirectionPeriod = 1;
+		randomDirectionIntensity = 0.04;
+		onTimerScript = "";
+		beforeDestroyScript = "";
+		destroyOnWaterSurface = 1;
+		destroyOnWaterSurfaceOffset = -0.6;
+		lifeTimeVar = 2;
+		positionVar[] = {0,0,0};
+		MoveVelocityVar[] = {0.25,0.25,0.25};
+		rotationVelocityVar = 20;
+		sizeVar = 0.5;
+		colorVar[] = {0,0,0,0.35};
+		randomDirectionPeriodVar = 0;
+		randomDirectionIntensityVar = 0;
+		blockAIVisibility=1;
+	};
+};
---
+++ addons/smoke/script_component.hpp
@@ -0,0 +1,12 @@
+#define COMPONENT smoke
+#include "\z\a3cs\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_SMOKE
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_SMOKE
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_CORE
+#endif
+
+#include "\z\a3cs\addons\main\script_macros.hpp"
