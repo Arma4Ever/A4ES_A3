5eda72bee3a951652df58e2f605f69109627aff5
SzwedzikPL <szwedzikpl@gmail.com>
2016-01-16 14:15:01 +0100

    restore advancedWounds

--- addons/main/ace_options.hpp
+++ addons/main/ace_options.hpp
@@ -181,7 +181,7 @@ class ACE_Settings {
         typeName = "SCALAR";
     };
     class ace_medical_enableAdvancedWounds {
-        value = 0; //no
+        value = 1; //yes
         typeName = "BOOL";
     };
     class ace_medical_medicSetting_PAK {
