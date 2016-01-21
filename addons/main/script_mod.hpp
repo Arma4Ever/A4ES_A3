940457e987ab3b2756520dd2007f22d1cbb6a48c
SzwedzikPL <szwedzikpl@gmail.com>
2016-01-21 21:15:55 +0100

    bump 0.6.6

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -5,7 +5,7 @@
 
 #define MAJOR 0
 #define MINOR 6
-#define PATCHLVL 5
+#define PATCHLVL 6
 #define BUILD 0
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
@@ -14,8 +14,8 @@
 #define ACE_TAG A.C.E.
 #define A3CS_TAG A3CS
 
-#define A3CS_VERSION 0.6.5
-#define A3CS_VERSION_ARRAY {0,6,5}
+#define A3CS_VERSION 0.6.6
+#define A3CS_VERSION_ARRAY {0,6,6}
 
 #define REQUIRED_VERSION 1.54
 
