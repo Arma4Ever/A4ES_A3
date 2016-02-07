c8232fd2b7c2449e207634f3aa78d0456a909735
SzwedzikPL <szwedzikpl@gmail.com>
2016-02-07 20:05:15 +0100

    delete cache module, moving to MM

--- addons/cache/CfgVehicles.hpp
+++
@@ -1,7 +0,0 @@
-class CfgVehicles {
-    class Module_F;
-    class A3C_Module: Module_F {
-        class ModuleDescription;
-    };
-    #include "modules\module_cacheSettings.hpp"
-};
--- addons/cache/functions/script_component.hpp
+++
@@ -1,1 +0,0 @@
-#include "\z\a3cs\addons\cache\script_component.hpp"
