530cb867a7a6be7d91c36610ade13c0d637281fc
SzwedzikPL <szwedzikpl@gmail.com>
2016-02-24 19:51:00 +0100

    noanimals cleanup

---
+++ addons/noanimals/CfgWorlds.hpp
@@ -0,0 +1,13 @@
+class CfgWorlds {
+    class CAWorld;
+    class Stratis: CAWorld {
+        class AmbientA3 {
+            maxCost = 0;
+        };
+    };
+    class Altis: CAWorld {
+        class AmbientA3 {
+            maxCost = 0;
+        };
+    };
+};
--- addons/noanimals/config.cpp
+++ addons/noanimals/config.cpp
@@ -12,16 +12,5 @@ class CfgPatches {
         authorUrl = "http://arma3coop.pl";
     };
 };
-class CfgWorlds {
-    class CAWorld;
-    class Stratis: CAWorld {
-        class AmbientA3 {
-            maxCost = 0;
-        };
-    };
-    class Altis: CAWorld {
-        class AmbientA3 {
-            maxCost = 0;
-        };
-    };
-};
+
+#include "CfgWorlds.hpp"
