788d5847a2950b39c8bc217e09c9022215a2c259
SzwedzikPL <szwedzikpl@gmail.com>
2016-02-25 17:09:01 +0100

    weaponactions preStart

---
+++ addons/weaponactions/XEH_PREP.hpp
@@ -0,0 +1,7 @@
+PREP(finishWeaponAction);
+PREP(getWeaponActionsChildren);
+PREP(startWeaponAction);
+PREP(checkDivingGearAction);
+PREP(startDivingGearSwitch);
+PREP(finishDivingGearSwitch);
+PREP(handleFired);
