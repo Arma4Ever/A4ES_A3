912db7a04190fde87b906805f3c0dd73adae91a6
SzwedzikPL <szwedzikpl@gmail.com>
2016-02-25 05:51:58 +0100

    jtac cleanup

--- addons/jtac/config.cpp
+++ addons/jtac/config.cpp
@@ -13,33 +13,33 @@ class CfgPatches {
     };
 };
 class a3cs_jtac_settings {
-	class support_providers {
+    class support_providers {
 
-	};
-	class support_types {
-		class air_gunrun {
-			displayName = "Ostrzał bezpośredni";
-			guidance = "laser";
-		};
-		class air_precision_bomb {
-			displayName = "Bomba kierowana";
-			guidance = "laser";
-		};
-		class cruise_missile {
-			displayName = "Pocisk manewrujący";
-			guidance = "laser";
-		};
-		class mortar_barrage {
-			displayName = "Ostrzał obszarowy";
-		};
-		class artillery_barrage {
-			displayName = "Ostrzał obszarowy";
-		};
-		class smoke_artillery_barrage {
-			displayName = "Ostrzał obszarowy (dymny)";
-		};
-		class 120mm_missiles_barrage {
+    };
+    class support_types {
+        class air_gunrun {
+            displayName = "Ostrzał bezpośredni";
+            guidance = "laser";
+        };
+        class air_precision_bomb {
+            displayName = "Bomba kierowana";
+            guidance = "laser";
+        };
+        class cruise_missile {
+            displayName = "Pocisk manewrujący";
+            guidance = "laser";
+        };
+        class mortar_barrage {
+            displayName = "Ostrzał obszarowy";
+        };
+        class artillery_barrage {
+            displayName = "Ostrzał obszarowy";
+        };
+        class smoke_artillery_barrage {
+            displayName = "Ostrzał obszarowy (dymny)";
+        };
+        class 120mm_missiles_barrage {
 
-		};
-	};
+        };
+    };
 };
