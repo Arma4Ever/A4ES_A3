7dc8c0c89c42e235d1bd53fc8ba5b1a245b2f0a6
SzwedzikPL <szwedzikpl@gmail.com>
2016-02-25 14:33:21 +0100

    gau8 prestart

--- addons/gau8/CfgEventHandlers.hpp
+++ addons/gau8/CfgEventHandlers.hpp
@@ -1,9 +1,13 @@
+class Extended_PreStart_EventHandlers {
+    class ADDON {
+        init = QUOTE(call COMPILE_FILE(XEH_preStart));
+    };
+};
 class Extended_PreInit_EventHandlers {
     class ADDON {
         init = QUOTE( call COMPILE_FILE(XEH_preInit) );
     };
 };
-
 class Extended_FiredBIS_EventHandlers {
     class Plane {
         class ADDON {
---
+++ addons/gau8/XEH_PREP.hpp
@@ -0,0 +1,1 @@
+PREP(gau8_muzzleEffect);
