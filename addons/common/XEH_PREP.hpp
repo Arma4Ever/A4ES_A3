5bb309f51a313ea86d3c748cc20661649c1eda63
SzwedzikPL <szwedzikpl@gmail.com>
2016-02-25 14:30:33 +0100

    common prestart

---
+++ addons/common/XEH_PREP.hpp
@@ -0,0 +1,11 @@
+PREP(resetScoreLoop);
+PREP(exportAddons);
+PREP(exportSettings);
+PREP(exportFunction);
+PREP(getName);
+PREP(getMissionData);
+PREP(getSingleMissionData);
+PREP(showNotification);
+PREP(handleKilled);
+PREP(handleHitPart);
+PREP(secondsToTime);
