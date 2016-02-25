7567814dae7d62444b52fde41573fad15dc904e0
SzwedzikPL <szwedzikpl@gmail.com>
2016-02-25 05:30:01 +0100

    boxes cleanup

---
+++ addons/boxes/CfgEventHandlers.hpp
@@ -0,0 +1,12 @@
+class Extended_InitPost_EventHandlers {
+    class MetalCase_01_base_F {
+        class ADDON {
+            init = QUOTE(_this call ace_cargo_fnc_initObject;_this call ace_dragging_fnc_initObject);
+        };
+    };
+    class PlasticCase_01_base_F {
+        class ADDON {
+            init = QUOTE(_this call ace_cargo_fnc_initObject;_this call ace_dragging_fnc_initObject);
+        };
+    };
+};
