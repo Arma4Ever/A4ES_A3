38c321badc9ba6c1b2858610fb1e67e498bd7bb7
SzwedzikPL <szwedzikpl@gmail.com>
2016-02-27 16:37:56 +0100

    bump 0.6.7

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -5,7 +5,7 @@
 
 #define MAJOR 0
 #define MINOR 6
-#define PATCHLVL 6
+#define PATCHLVL 7
 #define BUILD 0
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
@@ -14,10 +14,10 @@
 #define ACE_TAG A.C.E.
 #define A3CS_TAG A3CS
 
-#define A3CS_VERSION 0.6.6
-#define A3CS_VERSION_ARRAY {0,6,6}
+#define A3CS_VERSION 0.6.7
+#define A3CS_VERSION_ARRAY {0,6,7}
 
 #define REQUIRED_VERSION 1.56
 
 //wylacz debug przed release!
-//#define A3CS_DEBUG_ENABLED
+#define A3CS_DEBUG_ENABLED
