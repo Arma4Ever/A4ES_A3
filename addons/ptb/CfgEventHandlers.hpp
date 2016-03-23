94272759db792f7c57046c5d535637c3106e2548
SzwedzikPL <szwedzikpl@gmail.com>
2016-03-23 22:47:53 +0100

    merge postInits fixes

--- addons/ptb/CfgEventHandlers.hpp
+++ addons/ptb/CfgEventHandlers.hpp
@@ -10,7 +10,7 @@ class Extended_PreInit_EventHandlers {
 };
 class Extended_PostInit_EventHandlers {
     class ADDON {
-        init = QUOTE(call COMPILE_FILE(XEH_postInitClient));
+        init = QUOTE(call COMPILE_FILE(XEH_postInit));
     };
 };
 class Extended_InitPost_EventHandlers {
