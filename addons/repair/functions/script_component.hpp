9ee873662eba0a01bf87ba06151b093e840ea243
SzwedzikPL <szwedzikpl@gmail.com>
2016-06-03 22:47:38 +0200

    repair module, move repair actions to ace_repair main action

---
+++ addons/repair/functions/script_component.hpp
@@ -0,0 +1,1 @@
+#include "\z\a3cs\addons\repair\script_component.hpp"
---
+++ addons/repair/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT repair
+#include "\z\a3cs\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_REPAIR
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_REPAIR
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_REPAIR
+#endif
+
+#include "\z\a3cs\addons\main\script_macros.hpp"
