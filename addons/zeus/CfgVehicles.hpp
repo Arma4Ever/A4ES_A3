a3d95ce15f07d90217442e7f5daae68c4665d609
SzwedzikPL <szwedzikpl@gmail.com>
2016-06-11 21:53:52 +0200

    hide side zeus slots, hide "bad" curator addons options

---
+++ addons/zeus/CfgVehicles.hpp
@@ -0,0 +1,54 @@
+class CfgVehicles {
+    class Logic;
+    class Module_F: Logic {
+        class AttributesBase {
+            class Edit;
+            class Combo;
+            class Checkbox;
+            class CheckboxNumber;
+            class ModuleDescription;
+        };
+        class ModuleDescription {
+            class Anything;
+            class AnyPlayer;
+            class AnyObject;
+            class EmptyDetector;
+        };
+    };
+    class ModuleCurator_F: Module_F {
+        class Attributes: AttributesBase {
+            class Addons: Combo {
+                displayName = "$STR_A3_CfgVehicles_ModuleCurator_F_Arguments_Addons";
+                tooltip = "$STR_A3_CfgVehicles_ModuleCurator_F_Arguments_Addons_description";
+                property = "ModuleCurator_F_Addons";
+                defaultValue = 2;
+                typeName = "NUMBER";
+                class Values {
+                    delete AllActive;
+                    delete AllMission;
+                    delete None;
+
+                    class All {
+                        name = "$STR_A3_CfgVehicles_ModuleCurator_F_Arguments_Addons_values_All";
+                        value = 3;
+                        default = 1;
+                    };
+                };
+            };
+        };
+    };
+
+    class VirtualCurator_F;
+    class B_VirtualCurator_F: VirtualCurator_F {
+        scope = 1;
+    };
+    class O_VirtualCurator_F: VirtualCurator_F {
+        scope = 1;
+    };
+    class I_VirtualCurator_F: VirtualCurator_F {
+        scope = 1;
+    };
+    class C_VirtualCurator_F: VirtualCurator_F {
+        scope = 1;
+    };
+};
