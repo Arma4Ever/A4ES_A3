93a546aad609c8bb9efcc1c0397fa164e313abe2
SzwedzikPL <szwedzikpl@gmail.com>
2016-06-11 22:01:12 +0200

    tmp disable own dead cam

--- addons/common/CfgRespawnTemplates.hpp
+++ addons/common/CfgRespawnTemplates.hpp
@@ -1,6 +1,6 @@
 class CfgRespawnTemplates {
     class None {
-        onPlayerKilled = "a3cs_respawn_fnc_respawnOnPlayerKilled";
+        //onPlayerKilled = "a3cs_respawn_fnc_respawnOnPlayerKilled";
         //onPlayerRespawn = "a3cs_respawn_fnc_respawnOnPlayerRespawn";
     };
 };
