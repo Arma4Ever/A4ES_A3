4ad814aa8f9af27f21e7999fd4915601cc058710
SzwedzikPL <szwedzikpl@gmail.com>
2016-07-18 22:04:00 +0200

    bump 0.7.4

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -5,14 +5,14 @@
 
 #define MAJOR 0
 #define MINOR 7
-#define PATCHLVL 3
+#define PATCHLVL 4
 #define BUILD 0
 
 #define VERSION MAJOR.MINOR.PATCHLVL
 #define VERSION_AR MAJOR,MINOR,PATCHLVL
 
-#define REQUIRED_VERSION 1.6
-#define REQUIRED_CBA_VERSION {2,4,1}
+#define REQUIRED_VERSION 1.62
+#define REQUIRED_CBA_VERSION {3,0,0}
 
 #define AUTHOR_CONFIG \
     author = "$STR_A3CS_Main_ModAuthor"; \
