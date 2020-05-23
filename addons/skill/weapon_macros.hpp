a651805343611de1c91387c9d366aa67558dfa98
SzwedzikPL <szwedzikpl@gmail.com>
2020-05-23 10:16:47 +0200

    Remove skill addon

--- addons/skill/weapon_macros.hpp
+++
@@ -1,8 +0,0 @@
-
-#define A3CS_GRENADE(CNAME, PNAME) \
-    class ##CNAME##: ##PNAME## \
-    { \
-        aiAmmoUsageFlags = 32; \
-        allowAgainstInfantry = 0; \
-        cost = 1000000; \
-    }
