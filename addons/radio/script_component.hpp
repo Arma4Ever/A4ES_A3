5422390d2ebb408498ef3ae7f1c39a67c0d05fb7
SzwedzikPL <szwedzikpl@gmail.com>
2020-06-26 20:51:21 +0200

    Cleanup radio addon

--- addons/radio/script_component.hpp
+++ addons/radio/script_component.hpp
@@ -2,7 +2,7 @@
 #define COMPONENT_BEAUTIFIED Radio
 #include "\z\a3cs\addons\main\script_mod.hpp"
 
-#define DEBUG_MODE_FULL
+// #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
