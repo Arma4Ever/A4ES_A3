6b92ee7adc051469f701b79fe16b1ffe0ae6b401
SzwedzikPL <szwedzikpl@gmail.com>
2021-05-01 11:50:11 +0200

    Add cameras addon

---
+++ addons/cameras/CfgMagazines.hpp
@@ -0,0 +1,8 @@
+class CfgMagazines {
+	class CA_Magazine;
+	class Hates_Camera: CA_Magazine {
+        scope = 0;
+        scopeArsenal = 0;
+        scopeCurator = 0;
+	};
+};
---
+++ addons/cameras/README.md
@@ -0,0 +1,6 @@
+# A3CS Cameras
+Config tweaks for Hate's Hidden Cameras
+
+### Changes
+- Fixes camera view in UAV preview
+- Adds cameras for each faction
