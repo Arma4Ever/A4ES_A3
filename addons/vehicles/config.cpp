7fb20c6fa6ee893aa3387a803247f96e90b92208
SzwedzikPL <szwedzikpl@gmail.com>
2021-05-21 01:45:22 +0200

    Add vehicle addon; Tweak radarTargetSize for stealth planes;

---
+++ addons/vehicles/config.cpp
@@ -0,0 +1,21 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "a3cs_common",
+            "A3_Air_F_Jets_Plane_Fighter_01",
+            "A3_Air_F_Jets_Plane_Fighter_02"
+        };
+        author = ECSTRING(main,Author);
+        authors[] = {"SzwedzikPL"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgVehicles.hpp"
