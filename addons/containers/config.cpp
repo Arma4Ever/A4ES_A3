c0890b5202718d719cf231eef70f2a88e30e044b
SzwedzikPL <szwedzikpl@gmail.com>
2021-06-15 20:08:31 +0200

    Add 50k cargo, crates and 15k lockers to containers;

--- addons/containers/config.cpp
+++ addons/containers/config.cpp
@@ -6,11 +6,46 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {};
+        requiredAddons[] = {
+            "A3_Structures_F_Heli_Items_Luggage",
+            "A3_expEden",
+            "A3_Props_F_Enoch_Military_Supplies",
+            "A3_Props_F_Orange_Humanitarian_Supplies",
+            "A3_Missions_F_Oldman",
+            "A3_Supplies_F_Exp_Ammoboxes",
+            "A3_Weapons_F_Ammoboxes",
+            "A3_Supplies_F_Enoch_Ammoboxes",
+            "A3_Weapons_F_Bootcamp_Ammoboxes",
+            "A3_Supplies_F_Orange_Ammoboxes",
+            "A3_Supplies_F_Heli_CargoNets",
+            "A3_Structures_F_EPA_Mil_Scrapyard",
+            "A3_Structures_F_Ind_Cargo",
+            "A3_Props_F_Exp_Commercial_Market",
+            "ace_medical_treatment",
+            "plp_containers"
+        };
         author = ECSTRING(main,Author);
         authors[] = {"SzwedzikPL"};
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
+    };
+};
+
+class CfgEditorCategories {
+    class GVAR(edCat) {
+        displayName = "A3CS Kontenery";
+    };
+};
+
+class CfgEditorSubcategories {
+    class GVAR(crates_50k) {
+        displayName = "Skrzynki 50k";
+    };
+    class GVAR(cargo_50k) {
+        displayName = "Cargo 50k";
+    };
+    class GVAR(lockers_15k) {
+        displayName = "Szafki osobiste 15k";
     };
 };
 
--- addons/containers/script_component.hpp
+++ addons/containers/script_component.hpp
@@ -7,3 +7,14 @@
 // #define ENABLE_PERFORMANCE_COUNTERS
 
 #include "\z\a3cs\addons\main\script_macros.hpp"
+
+#define A3CS_CONTAINER(CLASSNAME,SIZE,SUBCAT) \
+class CLASSNAME; \
+class GVAR(CLASSNAME): CLASSNAME { \
+    editorCategory = QGVAR(edCat); \
+    editorSubcategory = QGVAR(SUBCAT); \
+    transportMaxBackpacks = SIZE; \
+    transportMaxMagazines = SIZE; \
+    transportMaxWeapons = SIZE; \
+    maximumLoad = SIZE; \
+}
