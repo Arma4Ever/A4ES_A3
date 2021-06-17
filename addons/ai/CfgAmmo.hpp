7b2aeae0580b4f5e71b1ebca695a3a36a390a664
SzwedzikPL <szwedzikpl@gmail.com>
2021-06-17 11:22:18 +0200

    Tweak ammo RPG ammo config for AI

--- addons/ai/CfgAmmo.hpp
+++ addons/ai/CfgAmmo.hpp
@@ -11,6 +11,7 @@ class CfgAmmo {
     class M_SPG9_HEAT: RocketBase {
         aiAmmoUsageFlags = "64 + 128 + 512";
         allowAgainstInfantry = 1;
+        cost = 20;
     };
     class R_PG32V_F: RocketBase {
         aiAmmoUsageFlags = "64 + 128 + 256 + 512";
@@ -27,16 +28,16 @@ class CfgAmmo {
     class R_MRAAWS_HEAT_F : RocketBase {
         aiAmmoUsageFlags = "64 + 128 + 512";
         allowAgainstInfantry = 1;
-        cost = 40;
+        cost = 20;
     };
     class R_MRAAWS_HEAT55_F: R_MRAAWS_HEAT_F {
         aiAmmoUsageFlags = "64 + 128 + 512";
         allowAgainstInfantry = 1;
-        cost = 40;
+        cost = 20;
     };
     class R_MRAAWS_HE_F: R_MRAAWS_HEAT_F {
         aiAmmoUsageFlags = "64 + 128 + 512";
         allowAgainstInfantry = 1;
-        cost = 40;
+        cost = 20;
     };
 };
