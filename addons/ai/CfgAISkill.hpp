c7f7450292e3d765db31b2d43c1e591bd383b4cf
SzwedzikPL <szwedzikpl@gmail.com>
2021-06-25 16:49:15 +0200

    Tweak AI skill settings

--- addons/ai/CfgAISkill.hpp
+++ addons/ai/CfgAISkill.hpp
@@ -1,8 +1,8 @@
 
-class CfgAISkill {                            // Skil 50% (default)
-    aimingAccuracy[] = {0, 0.055, 1, 0.365};  // 0.21
+class CfgAISkill {                            // Skill 50% (default)
+    aimingAccuracy[] = {0, 0.075, 1, 0.4};    // 0.2375
     aimingShake[] = {0, 0.25, 1, 0.6};        // 0.4
-    aimingSpeed[] = {0, 0.35, 1, 1};          // 0.675
+    aimingSpeed[] = {0, 0.4, 1, 1};           // 0.7
 
     spotDistance[] = {0, 0.7, 1, 1};          // 0.85
     spotTime[] = {0, 0.7, 1, 1};              // 0.85
