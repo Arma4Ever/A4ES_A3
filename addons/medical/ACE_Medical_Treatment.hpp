54fb98ab572e25f1e355aa17fa904337ceac8674
SzwedzikPL <szwedzikpl@gmail.com>
2022-03-07 16:32:19 +0100

    Tweak A3CS_painkillers treatment config

--- addons/medical/ACE_Medical_Treatment.hpp
+++ addons/medical/ACE_Medical_Treatment.hpp
@@ -1,13 +1,14 @@
 class ACE_Medical_Treatment {
     class Medication {
         class A3CS_painkillers {
-            painReduce = 0.35;
+            painReduce = 0.20;
             hrIncreaseLow[] = {-5, -10};
             hrIncreaseNormal[] = {-5, -15};
             hrIncreaseHigh[] = {-5, -17};
             timeInSystem = 420;
+            timeTillMaxEffect = 60;
             maxDose = 6;
-            inCompatableMedication[] = {};
+            incompatibleMedication[] = {};
             viscosityChange = 5;
         };
     };
