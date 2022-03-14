1d9d9b85b3cabf124184992513361201d37802b0
SzwedzikPL <szwedzikpl@gmail.com>
2022-03-14 15:08:32 +0100

    Tweak AI spotTime, spotDistance and courage settings;

--- addons/ai/CfgAISkill.hpp
+++ addons/ai/CfgAISkill.hpp
@@ -4,10 +4,10 @@ class CfgAISkill {                            // Skill 50% (default)
     aimingShake[] = {0, 0.25, 1, 0.6};        // 0.4
     aimingSpeed[] = {0, 1, 1, 1};             // 1
 
-    spotDistance[] = {0, 0.7, 1, 1};          // 0.85
-    spotTime[] = {0, 1, 1, 1};                // 1
+    spotDistance[] = {0, 0.25, 1, 1};         // 0.625
+    spotTime[] = {0, 0.25, 1, 1};             // 0.625
 
-    courage[] = {0, 0.6, 1, 1};               // 0.8
+    courage[] = {0, 0.7, 1, 1};               // 0.85
 
     commanding[] = {0, 1, 1, 1};              // 1
     endurance[] = {0, 1, 1, 1};               // 1
