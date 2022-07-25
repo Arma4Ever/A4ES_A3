a988ac9d0ac04b81a8814b451c37ddc4d22d33c6
SzwedzikPL <szwedzikpl@gmail.com>
2022-07-25 13:56:20 +0200

    Remove vendor 3DEN attrs;

---
+++ addons/vendor/Cfg3DEN.hpp
@@ -0,0 +1,17 @@
+class Cfg3DEN {
+	class Object {
+		class AttributeCategories {
+			class StateSpecial {
+				class Attributes {
+					delete ObjectBoxMarker;
+					delete GlassStates;
+				};
+			};
+			class VehicleSystems {
+				class Attributes {
+					delete CarAlarm;
+				};
+			};
+		};
+	};
+};
