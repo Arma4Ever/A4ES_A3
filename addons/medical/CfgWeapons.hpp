6130f64191a1baf52b59c1624b81d4c11454caa0
SzwedzikPL <szwedzikpl@gmail.com>
2022-07-25 15:53:20 +0200

    Lower ACE body bag mass

--- addons/medical/CfgWeapons.hpp
+++ addons/medical/CfgWeapons.hpp
@@ -29,4 +29,10 @@ class CfgWeapons {
             mass = 1;
         };
     };
+
+    class ACE_bodyBag: ACE_ItemCore {
+        class ItemInfo: CBA_MiscItem_ItemInfo {
+            mass = 1;
+        };
+    };
 };
