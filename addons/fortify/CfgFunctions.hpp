c271c78fff2e5acb4ca7cd4fe263f31311b58783
SzwedzikPL <szwedzikpl@gmail.com>
2022-07-25 13:59:21 +0200

    Remove global hint about fortify budget update;

--- addons/fortify/CfgFunctions.hpp
+++ addons/fortify/CfgFunctions.hpp
@@ -5,6 +5,9 @@ class CfgFunctions {
             class canFortify {
                 file = QPATHTOF(functions\fnc_ace_canFortify.sqf);
             };
+            class updateBudget {
+                file = QPATHTOF(functions\fnc_ace_updateBudget.sqf);
+            };
         };
     };
 };
