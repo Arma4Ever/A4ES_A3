5c5b9e9be0b98bf2d3b7637d531face4372420b1
Krzyciu <63709320+Krzyciu@users.noreply.github.com>
2022-08-01 17:58:44 +0200

    Anti Ship Missiles
    Added NATO AGM-84 Harpoon and Chinese/Pakistani counterpart - C802AK Eagle Strike

--- addons/missiles/CfgMagazines.hpp
+++ addons/missiles/CfgMagazines.hpp
@@ -10,6 +10,7 @@ class CfgMagazines {
 
     class PylonRack_Missile_AGM_02_x1: magazine_Missile_AGM_02_x1 {};
 
+
     class GVAR(JASSM_magazine_x1): magazine_Missile_AGM_02_x1 {
         ammo = QGVAR(JASSM);
         author = "Dani (TCVM) / SzwedzikPL";
@@ -67,5 +68,68 @@ class CfgMagazines {
         displayName = CSTRING(JASSM_CLUSTER_MAG_x1);
         displayNameShort = CSTRING(JASSM_CLUSTER_MAG_short);
         pylonWeapon = QGVAR(JASSM_cluster_Launcher);
+    };
+
+    class GVAR(AGM84_magazine_x1): magazine_Missile_AGM_02_x1 {
+        ammo = QGVAR(AGM84);
+        author = "Krzyciu";
+        descriptionShort = CSTRING(AGM84_MAG_DESCR);
+        displayName = CSTRING(AGM84_MAG_x1);
+        displayNameShort = CSTRING(AGM84_MAG_short);
+    };
+    class GVAR(AGM84_pylonmissile_x1): PylonMissile_Missile_AGM_02_x1 {
+        ammo = QGVAR(AGM84);
+        author = "Krzyciu";
+        descriptionShort = CSTRING(AGM84_MAG_DESCR);
+        displayName = CSTRING(AGM84_MAG_x1);
+        displayNameShort = CSTRING(AGM84_MAG_short);
+        pylonWeapon = QGVAR(AGM84_Launcher);
+    };
+    class GVAR(AGM84_pylonRack_1Rnd): PylonRack_1Rnd_Missile_AGM_02_F {
+        ammo = QGVAR(AGM84);
+        author = "Krzyciu";
+        descriptionShort = CSTRING(AGM84_MAG_DESCR);
+        displayName = CSTRING(AGM84_MAG_x1);
+        displayNameShort = CSTRING(AGM84_MAG_short);
+        pylonWeapon = QGVAR(AGM84_Launcher_Plane);
+    };
+
+    class GVAR(AGM84_PylonRack_x1): PylonRack_Missile_AGM_02_x1 {
+        ammo = QGVAR(AGM84);
+        author = "Krzyciu";
+        descriptionShort = CSTRING(AGM84_MAG_DESCR);
+        displayName = CSTRING(AGM84_MAG_x1);
+        displayNameShort = CSTRING(AGM84_MAG_short);
+        pylonWeapon = QGVAR(AGM84_Launcher);
+    };
+
+    class 4Rnd_Missile_AGM_01_F;
+    class PylonRack_1Rnd_Missile_AGM_01_F: 4Rnd_Missile_AGM_01_F {};
+    class magazine_Missile_AGM_KH25_x1: VehicleMagazine {};
+    class PylonMissile_Missile_AGM_KH25_x1: magazine_Missile_AGM_KH25_x1 {};
+
+    class GVAR(C802AK_magazine_x1): magazine_Missile_AGM_KH25_x1 {
+        ammo = QGVAR(C802AK);
+        author = "Krzyciu";
+        descriptionShort = CSTRING(C802AK_MAG_DESCR);
+        displayName = CSTRING(C802AK_MAG_x1);
+        displayNameShort = CSTRING(C802AK_MAG_short);
+        pylonWeapon = QGVAR(C802AK_Launcher);
+    };
+    class GVAR(C802AK_pylonmissile_x1): PylonMissile_Missile_AGM_KH25_x1 {
+        ammo = QGVAR(C802AK);
+        author = "Krzyciu";
+        descriptionShort = CSTRING(C802AK_MAG_DESCR);
+        displayName = CSTRING(C802AK_MAG_x1);
+        displayNameShort = CSTRING(C802AK_MAG_short);
+        pylonWeapon = QGVAR(C802AK_Launcher);
+    };
+    class GVAR(C802AK_PylonRack_x1): PylonRack_1Rnd_Missile_AGM_01_F {
+        ammo = QGVAR(C802AK);
+        author = "Krzyciu";
+        descriptionShort = CSTRING(C802AK_MAG_DESCR);
+        displayName = CSTRING(C802AK_MAG_x1);
+        displayNameShort = CSTRING(C802AK_MAG_short);
+        pylonWeapon = QGVAR(C802AK_Launcher);
     };
 };
--- addons/missiles/CfgWeapons.hpp
+++ addons/missiles/CfgWeapons.hpp
@@ -64,4 +64,49 @@ class CfgWeapons {
         ace_laser_canSelect = 1;
         ace_laser_showHud = 1;
     };
+
+    class GVAR(AGM84_Launcher): weapon_AGM_65Launcher {
+        author = "Dani (TCVM) / Krzyciu";
+        displayname = CSTRING(AGM84);
+        magazines[] = {
+            QGVAR(AGM84_magazine_x1),
+            QGVAR(AGM84_pylonmissile_x1),
+            QGVAR(AGM84_PylonRack_x1),
+            QGVAR(AGM84_pylonRack_1Rnd)
+        };
+        weaponLockDelay = 0.5;
+        weaponLockSystem = 0;
+        ace_laser_canSelect = 1;
+        ace_laser_showHud = 1;
+    };
+
+    class GVAR(AGM84_Launcher_Plane): Missile_AGM_02_Plane_CAS_01_F {
+        author = "Dani (TCVM) / Krzyciu";
+        displayname = CSTRING(AGM84);
+        magazines[] = {
+            QGVAR(AGM84_magazine_x1),
+            QGVAR(AGM84_pylonmissile_x1),
+            QGVAR(AGM84_PylonRack_x1),
+            QGVAR(AGM84_pylonRack_1Rnd)
+        };
+        weaponLockDelay = 0.5;
+        weaponLockSystem = 0;
+        ace_laser_canSelect = 1;
+        ace_laser_showHud = 1;
+    };
+
+    class weapon_AGM_KH25Launcher: MissileLauncher {};
+    class GVAR(C802AK_Launcher): weapon_AGM_KH25Launcher {
+        author = "Dani (TCVM) / Krzyciu";
+        displayname = CSTRING(C802AK);
+        magazines[] = {
+            QGVAR(C802AK_magazine_x1),
+            QGVAR(C802AK_pylonmissile_x1),
+            QGVAR(C802AK_PylonRack_x1)
+        };
+        weaponLockDelay = 0.5;
+        weaponLockSystem = 0;
+        ace_laser_canSelect = 1;
+        ace_laser_showHud = 1;
+    };
 };
