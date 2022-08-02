322ddd3f02d021dbb0699e3e7c69d259773a2d7a
SzwedzikPL <szwedzikpl@gmail.com>
2022-08-02 08:26:57 +0200

    Add new flare fire modes; Tweak radar target size for stealth planes & helis;

--- addons/vehicles/CfgVehicles.hpp
+++ addons/vehicles/CfgVehicles.hpp
@@ -1,11 +1,16 @@
 class CfgVehicles {
     class Plane_Fighter_01_Base_F;
     class B_Plane_Fighter_01_Stealth_F: Plane_Fighter_01_Base_F {
-        radarTargetSize = 0.25; // 0.4
+        radarTargetSize = 0.2; // 0.4
     };
 
     class Plane_Fighter_02_Base_F;
     class O_Plane_Fighter_02_Stealth_F: Plane_Fighter_02_Base_F {
-        radarTargetSize = 0.35; // 0.6
+        radarTargetSize = 0.3; // 0.6
+    };
+
+    class Heli_Attack_01_base_F;
+    class Heli_Attack_01_dynamicLoadout_base_F: Heli_Attack_01_base_F {
+        radarTargetSize = 0.3; // 0.7;
     };
 };
