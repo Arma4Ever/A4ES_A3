df120b1289ce647c5b61610b2322a7cfdd8be451
SzwedzikPL <szwedzikpl@gmail.com>
2022-08-21 19:27:32 +0200

    Add viewdistance addon; Make viewdistance settings category always on top of the list;

---
+++ addons/viewdistance/CfgEventHandlers.hpp
@@ -0,0 +1,6 @@
+
+class Extended_PreInit_EventHandlers {
+    class ace_viewdistance {
+        init = QUOTE(call COMPILE_FILE(XEH_ace_preInit));
+    };
+};
---
+++ addons/viewdistance/config.cpp
@@ -0,0 +1,17 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"a3cs_common", "ace_viewdistance"};
+        author = ECSTRING(main,Author);
+        authors[] = {"SzwedzikPL"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
