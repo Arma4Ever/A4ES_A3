40dc21b84fb69c2d6d0549d5d108e615009ef611
Krzyciu <63709320+Krzyciu@users.noreply.github.com>
2022-08-26 19:56:00 +0200

    New vanilla planes variants (#95)
    * Squashed commit of the following:
    
    commit 0ab0c655457e4c38c7a44d6509cccb585dce5bb6
    Author: SzwedzikPL <szwedzikpl@gmail.com>
    Date:   Tue Aug 2 08:33:04 2022 +0200
    
        Disable debug in missiles addon;
    
    commit 21e6021a0788e8d5ae649f60daaec6518da0022e
    Author: SzwedzikPL <szwedzikpl@gmail.com>
    Date:   Tue Aug 2 08:30:03 2022 +0200
    
        Add ATACMS rocket system; Tweak missile radar target filtering; Add support for ships in doppler radar;
    
    commit 322ddd3f02d021dbb0699e3e7c69d259773a2d7a
    Author: SzwedzikPL <szwedzikpl@gmail.com>
    Date:   Tue Aug 2 08:26:57 2022 +0200
    
        Add new flare fire modes; Tweak radar target size for stealth planes & helis;
    
    commit 9ea8d13efd1c073f2ffee821a7eac9a1498651e9
    Author: Krzyciu <63709320+Krzyciu@users.noreply.github.com>
    Date:   Mon Aug 1 18:14:44 2022 +0200
    
        Add AGM-84 missiles (#94)
    
        * Anti Ship Missiles
    
        Added NATO AGM-84 Harpoon and Chinese/Pakistani counterpart - C802AK Eagle Strike
    
        * Trailing space
    
        * Update config.cpp
    
    * New planes variants
    
    Added:
    Su-35 - To-201 Shikra with reduced max speed
    
    J-10 - A-149 Gryphon with increased max speed/acceleration
    
    Jak-135 - To-199 Neophron with increased max speed/acceleration
    
    Su-25SM3 - CUP Su-25T with TI camera (Shkval)
    
    * Tabs
    
    * Update CfgVehicles.hpp
    
    * Update CfgVehicles.hpp
    
    * Remove CUP dependency

--- addons/vehicles/CfgVehicles.hpp
+++ addons/vehicles/CfgVehicles.hpp
@@ -13,4 +13,42 @@ class CfgVehicles {
     class Heli_Attack_01_dynamicLoadout_base_F: Heli_Attack_01_base_F {
         radarTargetSize = 0.3; // 0.7;
     };
+
+    class O_Plane_Fighter_02_F;
+    class A3CS_Plane_Su35: O_Plane_Fighter_02_F {
+    		maxSpeed = 1145;
+        displayName="Su-35";
+        hiddenSelectionsTextures[]=
+        {
+          "a3\air_f_jets\plane_fighter_02\data\Fighter_02_fuselage_01_Blue_co.paa",
+          "a3\air_f_jets\plane_fighter_02\data\Fighter_02_fuselage_02_Blue_co.paa",
+          "a3\air_f_jets\plane_fighter_02\data\Fighter_02_fuselage_01_Blue_co.paa",
+          "a3\air_f_jets\plane_fighter_02\data\Numbers\Fighter_02_number_02_co.paa",
+          "a3\air_f_jets\plane_fighter_02\data\Numbers\Fighter_02_number_00_co.paa",
+          "a3\air_f_jets\plane_fighter_02\data\Numbers\Fighter_02_number_01_co.paa"
+        };
+    };
+
+    class I_Plane_Fighter_04_F;
+    class A3CS_Plane_J10: I_Plane_Fighter_04_F {
+        maxSpeed = 1200;
+        displayName="J-10";
+        acceleration=210;
+        hiddenSelectionsTextures[]=
+        {
+          "a3\air_f_jets\plane_fighter_04\data\Fighter_04_fuselage_01_co.paa",
+          "a3\air_f_jets\plane_fighter_04\data\Fighter_04_fuselage_02_co.paa",
+          "a3\air_f_jets\plane_fighter_04\data\fighter_04_misc_01_co.paa",
+          "a3\air_f_jets\plane_fighter_04\data\Numbers\Fighter_04_number_04_ca.paa",
+          "a3\air_f_jets\plane_fighter_04\data\Numbers\Fighter_04_number_04_ca.paa",
+          "a3\air_f_jets\plane_fighter_04\data\Numbers\Fighter_04_number_08_ca.paa"
+        };
+    };
+
+    class O_Plane_CAS_02_dynamicLoadout_F;
+    class A3CS_Plane_Jak135: O_Plane_CAS_02_dynamicLoadout_F {
+        maxSpeed = 1100;
+        acceleration=200;
+        displayName="Jak-135";
+    };
 };
--- addons/vehicles/config.cpp
+++ addons/vehicles/config.cpp
@@ -11,10 +11,12 @@ class CfgPatches {
             "A3_Weapons_F",
             "A3_Air_F_Beta_Heli_Attack_01",
             "A3_Air_F_Jets_Plane_Fighter_01",
-            "A3_Air_F_Jets_Plane_Fighter_02"
+            "A3_Air_F_Jets_Plane_Fighter_02",
+            "A3_Air_F_Jets_Plane_Fighter_04",
+            "A3_Air_F_EPC_Plane_CAS_02"
         };
         author = ECSTRING(main,Author);
-        authors[] = {"SzwedzikPL", "Jonpas"};
+        authors[] = {"SzwedzikPL", "Jonpas", "Krzyciu"};
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
     };
