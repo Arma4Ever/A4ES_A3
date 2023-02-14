e763ae07508f8d666265dbc392dababed6d5ef41
SzwedzikPL <szwedzikpl@gmail.com>
2023-02-14 20:59:30 +0100

    Convert tabs to spaces in main menu configs

--- addons/main_menu/RscDisplayA4ESServerToken.hpp
+++ addons/main_menu/RscDisplayA4ESServerToken.hpp
@@ -3,108 +3,108 @@ class RscDisplayA4ESServerToken: RscStandardDisplay {
     onLoad = QUOTE(_this call (uiNamespace getVariable QQFUNC(onLoadDisplayToken)));
     onUnload = QUOTE(_this call (uiNamespace getVariable QQFUNC(onUnloadDisplayToken)));
 
-	class controlsbackground {
-		class Vignette: RscVignette {
-			idc = 114998;
-		};
-		class TileGroup: RscControlsGroupNoScrollbars {
-			idc = 115099;
-			x = "safezoneXAbs";
-			y = "safezoneY";
-			w = "safezoneWAbs";
-			h = "safezoneH";
-			class Controls {
-				class Background: RscText {
-					idc = 114999;
-					x = 0;
-					y = 0;
-					w = "safezoneWAbs";
-					h = "safezoneH";
-					colorBackground[]={0,0,0,0.8};
-				};
-			};
-		};
-		class TitleBackground: RscText {
-			idc = 1080;
-			x = "(1 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
-			y = "(15 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
-			w = "38 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-			colorBackground[] = {
-				"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.13])",
-				"(profilenamespace getvariable ['GUI_BCG_RGB_G',0.54])",
-				"(profilenamespace getvariable ['GUI_BCG_RGB_B',0.21])",
-				"(profilenamespace getvariable ['GUI_BCG_RGB_A',0.8])"
-			};
-		};
-		class MainBackground: RscText {
-			idc = 1081;
-			x = "(1 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
-			y = "(16.1 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
-			w = "38 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h = "6.8 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-			colorBackground[] = {0,0,0,0.69999999};
-		};
-	};
-	class controls {
-		delete B_OK;
-		delete B_Cancel;
+    class controlsbackground {
+        class Vignette: RscVignette {
+            idc = 114998;
+        };
+        class TileGroup: RscControlsGroupNoScrollbars {
+            idc = 115099;
+            x = "safezoneXAbs";
+            y = "safezoneY";
+            w = "safezoneWAbs";
+            h = "safezoneH";
+            class Controls {
+                class Background: RscText {
+                    idc = 114999;
+                    x = 0;
+                    y = 0;
+                    w = "safezoneWAbs";
+                    h = "safezoneH";
+                    colorBackground[]={0,0,0,0.8};
+                };
+            };
+        };
+        class TitleBackground: RscText {
+            idc = 1080;
+            x = "(1 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
+            y = "(15 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
+            w = "38 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+            colorBackground[] = {
+                "(profilenamespace getvariable ['GUI_BCG_RGB_R',0.13])",
+                "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.54])",
+                "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.21])",
+                "(profilenamespace getvariable ['GUI_BCG_RGB_A',0.8])"
+            };
+        };
+        class MainBackground: RscText {
+            idc = 1081;
+            x = "(1 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
+            y = "(16.1 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
+            w = "38 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            h = "6.8 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+            colorBackground[] = {0,0,0,0.69999999};
+        };
+    };
+    class controls {
+        delete B_OK;
+        delete B_Cancel;
 
-		class Title: RscTitle {
-			style = 0;
-			idc = 1000;
-			text = "Weryfikacja tokenem";
-			x = "(1 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
-			y = "(15 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
-			w = "15 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-		};
-		class TextToken: RscText {
-			idc = 1002;
-			text = "Token";
-			x = "(1.5 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
-			y = "(18 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
-			w = "5 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-		};
-		class ValueToken: RscEdit {
-			idc = 101;
-			x = "(6.5 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
-			y = "(18 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
-			w = "31 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-		};
-		class TextLoading: RscStructuredText {
-			idc = 1004;
-			shadow = 0;
-			text = "";
-			x = "(1.5 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
-			y = "(18 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
-			w = "31 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h = "4 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-		};
-		class SubTextToken: RscStructuredText {
-			idc = 1003;
-			shadow = 0;
-			text = "";
-			x = "(1.5 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
-			y = "(19.5 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
-			w = "31 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h = "3 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-		};
-		class ButtonOk: RscButtonMenuOK {
-			idc = 102;
-			x = "(32.75 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
-			y = "(23 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
-			w = "6.25 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-		};
-		class ButtonCancel: RscButtonMenuCancel {
-			idc = 103;
-			x = "(1 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
-			y = "(23 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
-			w = "6.25 * (((safezoneW / safezoneH) min 1.2) / 40)";
-			h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
-		};
-	};
+        class Title: RscTitle {
+            style = 0;
+            idc = 1000;
+            text = "Weryfikacja tokenem";
+            x = "(1 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
+            y = "(15 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
+            w = "15 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+        };
+        class TextToken: RscText {
+            idc = 1002;
+            text = "Token";
+            x = "(1.5 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
+            y = "(18 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
+            w = "5 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+        };
+        class ValueToken: RscEdit {
+            idc = 101;
+            x = "(6.5 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
+            y = "(18 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
+            w = "31 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+        };
+        class TextLoading: RscStructuredText {
+            idc = 1004;
+            shadow = 0;
+            text = "";
+            x = "(1.5 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
+            y = "(18 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
+            w = "31 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            h = "4 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+        };
+        class SubTextToken: RscStructuredText {
+            idc = 1003;
+            shadow = 0;
+            text = "";
+            x = "(1.5 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
+            y = "(19.5 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
+            w = "31 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            h = "3 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+        };
+        class ButtonOk: RscButtonMenuOK {
+            idc = 102;
+            x = "(32.75 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
+            y = "(23 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
+            w = "6.25 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+        };
+        class ButtonCancel: RscButtonMenuCancel {
+            idc = 103;
+            x = "(1 + 20) * (((safezoneW / safezoneH) min 1.2) / 40) + (safezoneX)";
+            y = "(23 - 18) * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) + (safezoneY + safezoneH - (((safezoneW / safezoneH) min 1.2) / 1.2))";
+            w = "6.25 * (((safezoneW / safezoneH) min 1.2) / 40)";
+            h = "1 * ((((safezoneW / safezoneH) min 1.2) / 1.2) / 25)";
+        };
+    };
 };
--- addons/main_menu/RscDisplayMain.hpp
+++ addons/main_menu/RscDisplayMain.hpp
@@ -3,7 +3,7 @@ class RscDisplayMain: RscStandardDisplay {
     // delete all Spotlights added directly to the display. (only saw it in WS CDLC so far)
     delete Spotlight;
 
-	class Controls {
+    class Controls {
         delete Spotlight1;
         delete Spotlight2;
         delete Spotlight3;
@@ -13,120 +13,120 @@ class RscDisplayMain: RscStandardDisplay {
         delete BackgroundSpotlightLeft;
         delete BackgroundSpotlightRight;
 
-		class A4ES_BackgroundSpotlight: RscPicture {
-			text = "#(argb,8,8,3)color(1,1,1,1)";
-			colorText[] = {0, 0, 0, 0.89999998};
-			x = "0.5 - (1.5 * 10) * (pixelW * pixelGridNoUIScale * 2) - 2 * (2 * pixelW)";
-			y = "0.5 - (10 / 2) * (pixelH * pixelGridNoUIScale * 2) - (2 * pixelH)";
-			w = "3 * 10 * (pixelW * pixelGridNoUIScale * 2) + 4 * (2 * pixelW)";
-			h = "(10) * (pixelH * pixelGridNoUIScale * 2) + 2 * (2 * pixelH)";
-		};
-		class A4ES_BackgroundSpotlightLeft: A4ES_BackgroundSpotlight {
-			text = "\a3\Ui_f\data\GUI\Rsc\RscDisplayMain\gradient_ca.paa";
-			angle = 180;
-			x = "safezoneX";
-			w = "(safezoneW - (3 * 10 * (pixelW * pixelGridNoUIScale * 2) + 4 * (2 * pixelW))) / 2";
-		};
-		class A4ES_BackgroundSpotlightRight: A4ES_BackgroundSpotlightLeft {
-			text = "\a3\Ui_f\data\GUI\Rsc\RscDisplayMain\gradient_ca.paa";
-			angle = 0;
-			x = "0.5 + (1.5 * 10) * (pixelW * pixelGridNoUIScale * 2) + 2 * (2 * pixelW)";
-		};
-		class A4ES_Spotlight1: RscMainMenuSpotlight {
-			idc = 71021;
-			x = "0.5 - (1.5 * 10) * (pixelW * pixelGridNoUIScale * 2) - (2 * pixelW)";
-			class Controls: Controls {
-				class GroupPicture: GroupPicture {
-					class Controls: Controls {
-						class Picture: Picture {
-							text = "\a3\Ui_f\data\GUI\Rsc\RscDisplayMain\spotlight_2_ca.paa";
-						};
-						class Video: Video {
-							text = "\a3\Ui_f\Video\spotlight_2.ogv";
-						};
-					};
-				};
-				class GroupHover: GroupHover {
-					class Controls: Controls {
-						class Overlay: Overlay {};
-						class LineLeft: LineLeft {};
-						class LineRight: LineRight {};
-						class LineTop: LineTop {
-							text = "Dołącz na serwer Arma4Ever";
-						};
-						class LineBottom: LineBottom {};
-					};
-				};
-				class TextBackground: TextBackground {};
-				class Text: Text {
-					text = "DOŁĄCZ";
-				};
-				class Button: Button {};
-			};
-		};
-		class A4ES_Spotlight2: RscControlsGroupNoScrollbars {
-			idc = 71020;
-			x = "0.5 - (0.5 * 10) * (pixelW * pixelGridNoUIScale * 2)";
-			y = "0.5 - (10 / 2) * (pixelH * pixelGridNoUIScale * 2)";
-			w = "10 * (pixelW * pixelGridNoUIScale * 2)";
-			h = "10 * (pixelH * pixelGridNoUIScale * 2)";
-			
-			class Controls {
-				class Picture: RscPictureKeepAspect {
-					idc = 1023;
-					text = QPATHTOF(data\spotlight_info_error_ca.paa);
-					x = 0;
-					y = 0;
-					w = "10 * (pixelW * pixelGridNoUIScale * 2)";
-					h = "10 * (pixelH * pixelGridNoUIScale * 2)";
-				};
-				class Text: RscStructuredText {
-					idc = 1028;
-					size = "1.00 * (pixelH * pixelGridNoUIScale * 2)";
-					shadow = 0;
-					x = 0;
-					y = 0;
-					w = "10 * (pixelW * pixelGridNoUIScale * 2)";
-					h = "10 * (pixelH * pixelGridNoUIScale * 2)";
-					class Attributes {
-						align = "center";
-						color = "#ffffff";
-						font = "PuristaBold";
-					};
-				};
-			};
-		};
-		class A4ES_Spotlight3: RscMainMenuSpotlight {
-			idc = 71022;
-			x = "0.5 + (0.5 * 10) * (pixelW * pixelGridNoUIScale * 2) + (2 * pixelW)";
-			class Controls: Controls {
-				class GroupPicture: GroupPicture {
-					class Controls: Controls {
-						class Picture: Picture {
-							text = "\a3\Ui_f\data\GUI\Rsc\RscDisplayMain\spotlight_3_ca.paa";
-						};
-						class Video: Video {
-							text = "\a3\Ui_f\Video\spotlight3.ogv";
-						};
-					};
-				};
-				class GroupHover: GroupHover {
-					class Controls: Controls {
-						class Overlay: Overlay {};
-						class LineLeft: LineLeft {};
-						class LineRight: LineRight {};
-						class LineTop: LineTop {
-							text = "Stwórz scenariusz";
-						};
-						class LineBottom: LineBottom {};
-					};
-				};
-				class TextBackground: TextBackground {};
-				class Text: Text {
-					text = "EDYTOR";
-				};
-				class Button: Button {};
-			};
-		};
+        class A4ES_BackgroundSpotlight: RscPicture {
+            text = "#(argb,8,8,3)color(1,1,1,1)";
+            colorText[] = {0, 0, 0, 0.89999998};
+            x = "0.5 - (1.5 * 10) * (pixelW * pixelGridNoUIScale * 2) - 2 * (2 * pixelW)";
+            y = "0.5 - (10 / 2) * (pixelH * pixelGridNoUIScale * 2) - (2 * pixelH)";
+            w = "3 * 10 * (pixelW * pixelGridNoUIScale * 2) + 4 * (2 * pixelW)";
+            h = "(10) * (pixelH * pixelGridNoUIScale * 2) + 2 * (2 * pixelH)";
+        };
+        class A4ES_BackgroundSpotlightLeft: A4ES_BackgroundSpotlight {
+            text = "\a3\Ui_f\data\GUI\Rsc\RscDisplayMain\gradient_ca.paa";
+            angle = 180;
+            x = "safezoneX";
+            w = "(safezoneW - (3 * 10 * (pixelW * pixelGridNoUIScale * 2) + 4 * (2 * pixelW))) / 2";
+        };
+        class A4ES_BackgroundSpotlightRight: A4ES_BackgroundSpotlightLeft {
+            text = "\a3\Ui_f\data\GUI\Rsc\RscDisplayMain\gradient_ca.paa";
+            angle = 0;
+            x = "0.5 + (1.5 * 10) * (pixelW * pixelGridNoUIScale * 2) + 2 * (2 * pixelW)";
+        };
+        class A4ES_Spotlight1: RscMainMenuSpotlight {
+            idc = 71021;
+            x = "0.5 - (1.5 * 10) * (pixelW * pixelGridNoUIScale * 2) - (2 * pixelW)";
+            class Controls: Controls {
+                class GroupPicture: GroupPicture {
+                    class Controls: Controls {
+                        class Picture: Picture {
+                            text = "\a3\Ui_f\data\GUI\Rsc\RscDisplayMain\spotlight_2_ca.paa";
+                        };
+                        class Video: Video {
+                            text = "\a3\Ui_f\Video\spotlight_2.ogv";
+                        };
+                    };
+                };
+                class GroupHover: GroupHover {
+                    class Controls: Controls {
+                        class Overlay: Overlay {};
+                        class LineLeft: LineLeft {};
+                        class LineRight: LineRight {};
+                        class LineTop: LineTop {
+                            text = "Dołącz na serwer Arma4Ever";
+                        };
+                        class LineBottom: LineBottom {};
+                    };
+                };
+                class TextBackground: TextBackground {};
+                class Text: Text {
+                    text = "DOŁĄCZ";
+                };
+                class Button: Button {};
+            };
+        };
+        class A4ES_Spotlight2: RscControlsGroupNoScrollbars {
+            idc = 71020;
+            x = "0.5 - (0.5 * 10) * (pixelW * pixelGridNoUIScale * 2)";
+            y = "0.5 - (10 / 2) * (pixelH * pixelGridNoUIScale * 2)";
+            w = "10 * (pixelW * pixelGridNoUIScale * 2)";
+            h = "10 * (pixelH * pixelGridNoUIScale * 2)";
+            
+            class Controls {
+                class Picture: RscPictureKeepAspect {
+                    idc = 1023;
+                    text = QPATHTOF(data\spotlight_info_error_ca.paa);
+                    x = 0;
+                    y = 0;
+                    w = "10 * (pixelW * pixelGridNoUIScale * 2)";
+                    h = "10 * (pixelH * pixelGridNoUIScale * 2)";
+                };
+                class Text: RscStructuredText {
+                    idc = 1028;
+                    size = "1.00 * (pixelH * pixelGridNoUIScale * 2)";
+                    shadow = 0;
+                    x = 0;
+                    y = 0;
+                    w = "10 * (pixelW * pixelGridNoUIScale * 2)";
+                    h = "10 * (pixelH * pixelGridNoUIScale * 2)";
+                    class Attributes {
+                        align = "center";
+                        color = "#ffffff";
+                        font = "PuristaBold";
+                    };
+                };
+            };
+        };
+        class A4ES_Spotlight3: RscMainMenuSpotlight {
+            idc = 71022;
+            x = "0.5 + (0.5 * 10) * (pixelW * pixelGridNoUIScale * 2) + (2 * pixelW)";
+            class Controls: Controls {
+                class GroupPicture: GroupPicture {
+                    class Controls: Controls {
+                        class Picture: Picture {
+                            text = "\a3\Ui_f\data\GUI\Rsc\RscDisplayMain\spotlight_3_ca.paa";
+                        };
+                        class Video: Video {
+                            text = "\a3\Ui_f\Video\spotlight3.ogv";
+                        };
+                    };
+                };
+                class GroupHover: GroupHover {
+                    class Controls: Controls {
+                        class Overlay: Overlay {};
+                        class LineLeft: LineLeft {};
+                        class LineRight: LineRight {};
+                        class LineTop: LineTop {
+                            text = "Stwórz scenariusz";
+                        };
+                        class LineBottom: LineBottom {};
+                    };
+                };
+                class TextBackground: TextBackground {};
+                class Text: Text {
+                    text = "EDYTOR";
+                };
+                class Button: Button {};
+            };
+        };
     };
 };
