76c37359e55ee34892d23ffdd0ca91410de98b1a
Krzyciu <63709320+Krzyciu@users.noreply.github.com>
2023-03-03 00:39:27 +0100

    Ranks update (#101)
    * Ranks update
    
    - Fix UK ranks
    - Add Ukrainian ranks
    
    * Update ranks.hpp

--- addons/nametags/ranks/ww2uk/ranks.hpp
+++ addons/nametags/ranks/ww2uk/ranks.hpp
@@ -5,31 +5,46 @@ class WW2UK_szeregowy {
  };
 class WW2UK_kapral {
     name = "Kapral";
-    icon = QPATHTOF(ranks\ww2uk\corporal_gs.paa);
+    icon = QPATHTOF(ranks\ww2uk\lanceCorporal_gs.paa);
     importance = 1;
  };
-class WW2UK_sierzant {
+class WW2UK_st_kapral {
+    name = "St. Kapral";
+    icon = QPATHTOF(ranks\ww2uk\corporal_gs.paa);
+    importance = 2;
+ };
+ class WW2UK_sierzant {
     name = CSTRING(Ranks_Sierzant);
     icon = QPATHTOF(ranks\ww2uk\sergeant_gs.paa);
     importance = 3;
  };
+class WW2UK_st_sierzant {
+    name = CSTRING(Ranks_Sierzant_Sztabowy);
+    icon = QPATHTOF(ranks\ww2uk\colourSergeant_gs.paa);
+    importance = 4;
+ };
 class WW2UK_Podporucznik {
     name = "Podporucznik";
-    icon = QPATHTOF(ranks\ww2uk\lieutenant_gs.paa);
+    icon = QPATHTOF(ranks\ww2uk\2ndlieutenant_gs.paa);
     importance = 5;
+ };
+ class WW2UK_Porucznik {
+    name = "Porucznik";
+    icon = QPATHTOF(ranks\ww2uk\lieutenant_gs.paa);
+    importance = 6;
  };
 class WW2UK_Kapitan {
     name = "Kapitan";
     icon = QPATHTOF(ranks\ww2uk\captain_gs.paa);
-    importance = 5;
+    importance = 7;
  };
  class WW2UK_Major {
     name = "Major";
     icon = QPATHTOF(ranks\ww2uk\major_gs.paa);
-    importance = 5;
+    importance = 8;
  };
  class WW2UK_Pulkownik {
     name = CSTRING(Ranks_Pulkownik);
     icon = QPATHTOF(ranks\ww2uk\colonel_gs.paa);
-    importance = 5;
+    importance = 9;
  };
