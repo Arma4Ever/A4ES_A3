5c7616e67abc3810fd353f90b3dc5e5f3b0f1a3a
SzwedzikPL <szwedzikpl@gmail.com>
2023-07-23 18:33:15 +0200

    Adjust AI skill levels

--- addons/ai/CfgAISkill.hpp
+++ addons/ai/CfgAISkill.hpp
@@ -2,10 +2,10 @@
 class CfgAISkill {                            // Skill 50% (default)
     aimingAccuracy[] = {0, 0.075, 1, 0.5};    // 0.2875
     aimingShake[] = {0, 0.15, 1, 0.4};        // 0.275
-    aimingSpeed[] = {0, 1, 1, 1};             // 1
+    aimingSpeed[] = {0, 0.5, 1, 1};           // 0.75 // 1
 
-    spotDistance[] = {0, 0.25, 1, 1};         // 0.625
-    spotTime[] = {0, 0.25, 1, 1};             // 0.625
+    spotDistance[] = {0, 0.25, 0.75, 1};      // 0.5 // 0.625
+    spotTime[] = {0, 0.25, 0.5, 1};           // 0.375 // 0.625
 
     courage[] = {0, 0.7, 1, 1};               // 0.85
 
