e34d516321e0acee90a7cd6c29f67fbce7a0f0e7
Krzyciu <63709320+Krzyciu@users.noreply.github.com>
2026-03-09 18:58:34 +0100

    Tweaked: Increased flare fall time

--- addons/flares/CfgAmmo.hpp
+++ addons/flares/CfgAmmo.hpp
@@ -4,8 +4,8 @@ class CfgAmmo {
 
     class F_40mm_White: FlareBase {
         intensity = 500000; // 10000
-        timeToLive = 40; // 25
-        coefGravity = 0.25; // 1
+        timeToLive = 50; // 25
+        coefGravity = 0.2; // 1
     };
 
     class Flare_82mm_AMOS_White: FlareCore {
